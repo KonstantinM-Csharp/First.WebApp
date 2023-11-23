ae361dc4c5c70e1970d41dc6cccbd2437f8ebb61
Konstantin <forever_021@mail.ru>
2023-11-23 16:33:24 +0300

    Добавьте файлы проекта.

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Firsts.WebApp.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("maker")]
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Img { get; set; }
+        [JsonPropertyName("url")]
+        public  string  Url { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("description")]
+        public  string Description { get; set; }
+        [JsonPropertyName("ratings")]
+        public int[] Ratings { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+    }
+}
---
+++ Services/JsonFileProductsService.cs
@@ -0,0 +1,30 @@
+﻿using Firsts.WebApp.Models;
+using System.Text.Json;
+
+namespace Firsts.WebApp.Services
+{
+    public class JsonFileProductsService
+    {
+        public JsonFileProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
