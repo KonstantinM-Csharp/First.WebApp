ac9b2a85fed943ef5a3fd07326a1d8d22101c321
Konstantin <forever_021@mail.ru>
2023-11-24 20:27:46 +0300

    Первая фиксация

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Firsts.WebApp.Models;
+using Firsts.WebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Firsts.WebApp.Pages
@@ -6,15 +8,18 @@ namespace Firsts.WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService ProductsService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productsService)
         {
             _logger = logger;
+            ProductsService = productsService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductsService.GetProducts();
         }
     }
 }
