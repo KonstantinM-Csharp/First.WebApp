0e2d055ad13201475d340ac2a54079f354910f10
KonstantinM-Csharp <forever_021@mail.ru>
2023-11-29 22:44:19 +0300

    Фото для карточек

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Firsts.WebApp.Models;
+using Firsts.WebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Firsts.WebApp.Pages
@@ -6,15 +8,19 @@ namespace Firsts.WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService ProductService { get; set; }
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productService)
         {
             _logger = logger;
+            ProductService = productService;
+
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
